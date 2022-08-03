1e5345e8de0f4baf26194d5f0ad0136a62b1fdb9
dgstephenson <dgstephenson.econ@gmail.com>
2022-08-03 12:14:25 -0400

    Fix directions

--- src/server/lib/directions.ts
+++ src/server/lib/directions.ts
@@ -1,8 +1,8 @@
 export const NORTH = { x: 0, y: -1 }
-export const NORTH_EAST = { x: 0.5, y: -0.5 }
+export const NORTH_EAST = { x: Math.sqrt(0.5), y: -Math.sqrt(0.5) }
 export const EAST = { x: 1, y: 0 }
-export const SOUTH_EAST = { x: 0.5, y: 0.5 }
+export const SOUTH_EAST = { x: Math.sqrt(0.5), y: Math.sqrt(0.5) }
 export const SOUTH = { x: 0, y: 1 }
-export const SOUTH_WEST = { x: -0.5, y: 0.5 }
+export const SOUTH_WEST = { x: -Math.sqrt(0.5), y: Math.sqrt(0.5) }
 export const WEST = { x: -1, y: 0 }
-export const NORTH_WEST = { x: -0.5, y: -0.5 }
+export const NORTH_WEST = { x: -Math.sqrt(0.5), y: -Math.sqrt(0.5) }
