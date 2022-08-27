5166a10da8403e2cb2e387b852a5aef136ab670e
dgstephenson <dgstephenson.econ@gmail.com>
2022-08-27 14:59:28 -0400

    Adjust tick rate

--- src/server/index.ts
+++ src/server/index.ts
@@ -48,7 +48,7 @@ const io = new socketIo.Server<ClientToServerEvents, ServerToClientEvents>(serve
 const PORT = process.env.PORT ?? 3000
 server.listen(PORT, () => {
   console.log(`Listening on :${PORT}`)
-  setInterval(tick, 30)
+  setInterval(tick, 50)
 })
 
 async function updateClients (): Promise<void> {
